434e4bb7c481998988a09085532692eae7e67d88
unknown <tgelety@gmail.com>
2021-06-08 12:02:28 -0700

    just checking

--- db/seed.sql
+++ db/seed.sql
@@ -1,14 +1,19 @@
 INSERT INTO department (department_name)
 VALUES
     ('R&D'),
-    ('Janitorial')
+    ('Janitorial'),
+    ('Management');
 
 INSERT INTO roles (title_name, salary, department_id)
 VALUES
-    ('scientist', 15000, 1),
-    ('janitor', 10000, 2)
+    ('Scientist', 15000, 1),
+    ('Engineer', 14000, 1),
+    ('Janitor', 10000, 2),
+    ('CEO', 1000000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ('Gordan','Freeman', 1),
-    ('Albert','Einstien', 2, 1)
+    ('Gordon', 'Freeman', 1, 4),
+    ('Albert', 'Einstien', 3, 4),
+    ('Montgomery', 'Scott', 2, 4),
+    ('Jordan', 'Belfort', 3, 4);
